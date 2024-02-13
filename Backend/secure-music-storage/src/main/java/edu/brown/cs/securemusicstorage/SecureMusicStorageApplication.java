acbb1348b3245c3316b8967e41f225771ecdfa7c
SunZh <zhenhao_sun@brown.edu>
2024-02-13 13:20:56 -0500

    create spring template for backend

---
+++ Backend/secure-music-storage/src/main/java/edu/brown/cs/securemusicstorage/SecureMusicStorageApplication.java
@@ -0,0 +1,13 @@
+package edu.brown.cs.securemusicstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureMusicStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureMusicStorageApplication.class, args);
+    }
+
+}
